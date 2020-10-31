236be6a4590dc7d6079688ba476d30db4de88964
Ilyas Ghembaza <ilyas.ghembaza.ext@upply.com>
2020-10-31 23:58:14 +0100

    Refacto des cartes pour les films

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
+import Card from "../Card/Card";
 
 /*
 import "slick-carousel/slick/slick.css";
@@ -12,8 +13,8 @@ function SampleNextArrow(props) {
     const { onClick } = props;
     return (
         <button
-            className="slick-button-next"
-            onClick={onClick}
+        className="slick-button-next"
+        onClick={onClick}
         >
             <span className="slider-button-iconNext"></span>
         </button>
@@ -33,38 +34,30 @@ function SampleNextArrow(props) {
     );
   }
 
-  export default class Slide extends Component {
-render() {
-    const renderSlides = () =>
-    [1, 2, 3, 4, 5, 6, 7, 8].map(num => (
-        <div className="slider-movie">
-            <img 
-            src="https://image.tmdb.org/t/p/w440_and_h660_face/fkqJJDb4u9rEyJu8Jm8H1P2mux4.jpg"
-            className="slider-movie-img"
-            alt="img"
-            />
-            <h3 className="slider-movie-title">Slide {num}</h3>
-            <p className="slider-movie-year">2019</p>
-        </div>
-    ));
-    const settings = {
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 2,
-        speed: 1000,
-        width: 200,
-        nextArrow: <SampleNextArrow />,
-        prevArrow: <SamplePrevArrow />
-      };
-    return (
-        <section className="container">
-            <h1 className="top10-title">Les 10 meilleurs films</h1>
-            <div className="slider">
-            <Slider {...settings}>
-                {renderSlides()}
-            </Slider>
-            </div>
-        </section>
-    )
-}
+export default class Slide extends Component {
+    render() {
+        const renderSlides = () =>
+        [1, 2, 3, 4, 5, 6, 7, 8].map(num => (
+            <Card title={num} />
+        ));
+        const settings = {
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 2,
+            speed: 1000,
+            width: 200,
+            nextArrow: <SampleNextArrow />,
+            prevArrow: <SamplePrevArrow />
+        };
+        return (
+            <section className="container">
+                <h1 className="top10-title">Les 10 meilleurs films</h1>
+                <div className="slider">
+                <Slider {...settings}>
+                    {renderSlides()}
+                </Slider>
+                </div>
+            </section>
+        )
+    }
 }
