3c22d88bff49f4fbbaa5d966167892f754592e41
Ilyas Ghembaza <ilyas.ghembaza.ext@upply.com>
2020-10-31 17:04:31 +0100

    Creation du composant Slider

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Slider.css';
+
+function Slider() {
+    return (
+        <section className="container">
+            <h1 className="top10-title">Les 10 meilleurs films</h1>
+        </section>
+    )
+}
+
+export default Slider;
