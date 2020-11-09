97b729994aaa678f7728841e59dcd33c22ff9862
Ilyas Ghembaza <ilyas.ghembaza.ext@upply.com>
2020-11-09 21:05:59 +0100

    Séparation des select

--- src/containers/AllMoviesSection/AllMoviesSection.jsx
+++ src/containers/AllMoviesSection/AllMoviesSection.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import GendersFilter from './GendersFilter/GendersFilter'
 import SortByFilter from './SortByFilter/SortByFilter'
+import YearsFilter from './YearsFilter/YearsFilter'
 
 export default function AllMoviesSection() {
   return (
     <div>
       <SortByFilter />
       <GendersFilter />
+      <YearsFilter />
     </div>
   )
 }
--- src/containers/AllMoviesSection/GendersFilter/GendersFilter.jsx
+++ src/containers/AllMoviesSection/GendersFilter/GendersFilter.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Select from '../../../components/Select/Select'
+import Axios from 'axios';
+import { ALL_MOVIES_URL, END_POINT, GENRES_MOVIES_URL } from '../../../EndPoints'
 
 export default function GendersFilter() {
+  const [genres, setGenres] = useState([])
+  const [genreSelected, setGenreSelected] = useState(0);
+  const [movies, setMovies] = useState([])
+  const [moviesList, setMoviesList] = useState([])
+
+  const sortByGenre = (movies, element, value) => {
+    const result = movies.filter((movie) => movie[element].includes(parseInt(value)));
+    return result;
+}
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+        const res = await Axios.get(`${END_POINT}${ALL_MOVIES_URL}`)
+        const res2 = await Axios.get(`${END_POINT}${GENRES_MOVIES_URL}`)
+        const genres = res2.data.genres
+        setGenres(genres)
+    }
+    fetchMovies();
+}, []);
+
+  useEffect(() => {
+    console.log('genre changed', genreSelected)
+    const moviesSorted = sortByGenre(moviesList, "genre_ids", genreSelected);
+    setMovies(moviesSorted.length > 0 ? moviesSorted : [])
+}, [genreSelected]);
+
   return (
     <>
       <Select 
