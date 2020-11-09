c5f8ebfe32ed26206b9b2998d2ee16b2753c5f39
Ilyas Ghembaza <ilyas.ghembaza.ext@upply.com>
2020-11-09 22:00:48 +0100

    Ajout d'un eslintignore et MoviesList et TopMoviesList en commentaires car contient des children

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
 import Header from './components/Header/Header';
-import AllMoviesSection from './containers/AllMoviesSection/AllMoviesSection';
-import MoviesList from './containers/MoviesList/MoviesList';
+/* import MoviesList from './containers/MoviesList/MoviesList';
 import TopMoviesList from './containers/TopMoviesList';
+*/
 
 function App() {
   return (
     // eslint-disable-next-line react/jsx-filename-extension
     <div className="App">
       <Header />
-      <TopMoviesList />
-      <MoviesList />
     </div>
   );
 }
